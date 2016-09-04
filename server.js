d7414aa732cce264b1c432d51d76617005ce8b7e
Arjun U <ar5junu@gmail.com>
2016-09-04 21:07:07 +0530

    Project setup

---
+++ server.js
@@ -0,0 +1,13 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.dev.config.js');
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {colors: true},
+}).listen(5000, 'localhost', function (err) {
+    if (err)
+        console.log(err);
+    console.log('Listening at localhost:5000');
+});
